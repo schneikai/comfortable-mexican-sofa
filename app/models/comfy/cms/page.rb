a15218de3b58bad688d71fdaa860b09954c16110
Kai Schneider <schneikai@gmail.com>
2015-04-09 18:34:10 +0200

    update how translations are loaded

--- app/models/comfy/cms/page.rb
+++ app/models/comfy/cms/page.rb
@@ -67,33 +67,58 @@ class Comfy::Cms::Page < ActiveRecord::Base
   #   @cms_site.pages.find_page('/')
   #   @cms_site.pages.find_page('/', locale: :de, published: true)
   #
+  # def self.find_page(path, options={})
+  #   options = { locale: nil, published: true }.merge(options)
+
+  #   page = where(full_path: path)
+  #   page = page.where(is_published: true) if options[:published]
+  #   page = page.first
+
+  #   # If the page was not found try to find it via its translated paths.
+  #   # It will only return published translations if the published option is *true*.
+  #   # If a translation is found it won't check if the page itself is published though.
+  #   # That way you can fetch translated pages even if the default language page is
+  #   # not yet published.
+  #   if page.nil? && options[:locale]
+  #     translation = reflect_on_association(:translations).klass.where(full_path: path, locale: options[:locale])
+  #     translation = translation.where(is_published: true) if options[:published]
+  #     page = translation.first.try(:translateable)
+  #   end
+
+  #   # Try to translate the page.
+  #   # If a translation is found it is saved on the *translation* attribute.
+  #   # Methods like <tt>content_cache</tt> or <tt>target_page</tt> will then load
+  #   # content from that translation instead of from the page.
+  #   # It will only return published translations if the published option is *true*.
+  #   if page && options[:locale] && options[:locale].to_s != page.site.locale
+  #     translation = page.translations.where(locale: options[:locale])
+  #     translation = translation.where(is_published: true) if options[:published]
+  #     page.translation = translation.first
+  #   end
+
+  #   page
+  # end
+
+  # TODO CMS: New method that returns only pages or translations for the exact
+  # locale if a locale is given.
+  # Find a page for the given path.
+  # If a locale is given in the options the page locale (that is the page site
+  # locale in that case) must match. If no page is found it will try to find a
+  # translation matching the given path and locale.
   def self.find_page(path, options={})
     options = { locale: nil, published: true }.merge(options)
 
     page = where(full_path: path)
     page = page.where(is_published: true) if options[:published]
+    page = page.includes(:site).where("comfy_cms_sites.locale" => options[:locale]) if options[:locale].present?
     page = page.first
 
-    # If the page was not found try to find it via its translated paths.
-    # It will only return published translations if the published option is *true*.
-    # If a translation is found it won't check if the page itself is published though.
-    # That way you can fetch translated pages even if the default language page is
-    # not yet published.
-    if page.nil? && options[:locale]
-      translation = reflect_on_association(:translations).klass.where(full_path: path, locale: options[:locale])
-      translation = translation.where(is_published: true) if options[:published]
-      page = translation.first.try(:translateable)
-    end
-
-    # Try to translate the page.
-    # If a translation is found it is saved on the *translation* attribute.
-    # Methods like <tt>content_cache</tt> or <tt>target_page</tt> will then load
-    # content from that translation instead of from the page.
-    # It will only return published translations if the published option is *true*.
-    if page && options[:locale] && options[:locale].to_s != page.site.locale
-      translation = page.translations.where(locale: options[:locale])
-      translation = translation.where(is_published: true) if options[:published]
-      page.translation = translation.first
+    # If no page was found try to find one via translation.
+    # Translation objects act just like page objects so we just return the translation object here as a page.
+    if page.nil? && options[:locale].present?
+      page = reflect_on_association(:translations).klass.where(full_path: path, locale: options[:locale])
+      page = page.where(is_published: true) if options[:published]
+      page = page.first
     end
 
     page
@@ -107,17 +132,21 @@ class Comfy::Cms::Page < ActiveRecord::Base
     page
   end
 
-  # Will return translated content cache if translation is available. Otherwise
-  # original content cache.
-  def content_cache
-    translation ? translation.content_cache : super
-  end
+  # TODO CMS: Removed because we just return the translation object in find_page now.
+  # I did this because I use block_content resp. cms_block_content a lot and
+  # this would alos need to return translated content which it won't when accessd
+  # via the page object.
+  # # Will return translated content cache if translation is available. Otherwise
+  # # original content cache.
+  # def content_cache
+  #   translation ? translation.content_cache : super
+  # end
 
-  # Will return translated target page if translation is available. Otherwise
-  # original target page.
-  def target_page
-    translation ? translation.target_page : super
-  end
+  # # Will return translated target page if translation is available. Otherwise
+  # # original target page.
+  # def target_page
+  #   translation ? translation.target_page : super
+  # end
 
 protected
 
